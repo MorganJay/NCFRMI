49af8602367263cc19035554fa925d64087c2865
James Morgan <james.morgan@thebulb.africa>
2021-09-20 23:18:18 +0100

    updating template

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState, useEffect } from "react";
-import { Navigation } from "./components/navigation";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
-import { Contact } from "./components/contact";
+import { Switch, Route } from "react-router-dom";
+
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import DataCollection from "./pages/DataCollection";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -19,21 +14,24 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
 
 const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
+
   useEffect(() => {
     setLandingPageData(JsonData);
   }, []);
 
   return (
     <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <HomePage landingPageData={landingPageData} {...props} />
+          )}
+        />
+        <Route exact path="/auth"></Route>
+        <Route exact path="/form" component={DataCollection} />
+      </Switch>
     </div>
   );
 };
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,62 +1,76 @@
-export const Navigation = (props) => {
+import { Link } from "react-router-dom";
+
+export const Navigation = props => {
+  console.log(props);
   return (
-    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
-      <div className='container'>
-        <div className='navbar-header'>
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
           <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
           >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
           </button>
-          <a className='navbar-brand page-scroll' href='#page-top'>
-            React Landing Page
-          </a>{' '}
+          <a
+            className="navbar-brand page-scroll"
+            target="_blank"
+            rel="noreferrer"
+            href="https://ncfrmi.gov.ng/"
+          >
+            <img src={props?.data?.logo} alt="" height="64" width="64" className="img-responsive" />
+           
+          </a>{" "}
         </div>
 
         <div
-          className='collapse navbar-collapse'
-          id='bs-example-navbar-collapse-1'
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
         >
-          <ul className='nav navbar-nav navbar-right'>
+          <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href='#features' className='page-scroll'>
+              <a href="#features" className="page-scroll">
                 Features
               </a>
             </li>
             <li>
-              <a href='#about' className='page-scroll'>
+              <a href="#about" className="page-scroll">
                 About
               </a>
             </li>
             <li>
-              <a href='#services' className='page-scroll'>
+              <a href="#services" className="page-scroll">
                 Services
               </a>
             </li>
             <li>
-              <a href='#portfolio' className='page-scroll'>
+              <a href="#portfolio" className="page-scroll">
                 Gallery
               </a>
             </li>
             <li>
-              <a href='#testimonials' className='page-scroll'>
+              <a href="#testimonials" className="page-scroll">
                 Testimonials
               </a>
             </li>
             <li>
-              <a href='#team' className='page-scroll'>
+              <a href="#team" className="page-scroll">
                 Team
               </a>
             </li>
             <li>
-              <a href='#contact' className='page-scroll'>
+              <Link to="/form" className="page-scroll">
+                Data Collection
+              </Link>
+            </li>
+            <li>
+              <a href="#contact" className="page-scroll">
                 Contact
               </a>
             </li>
@@ -64,5 +78,5 @@ export const Navigation = (props) => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
